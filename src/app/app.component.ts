69342eb7b0133633a85d0ae12a935d4edbb28f68
“madeefarabbas” <madeefarabbas@gmail.com>
2022-08-06 01:41:35 +0400

    mock-server-angular-project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mock-server';
+  users: Observable<any> = this.http.get('http://localhost:3000/api/users')
+    .pipe(map((data: any) => {
+      return [...data.data.users]
+    }));
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.users.subscribe(data => {
+      console.log(data)
+    })
+  }
 }
